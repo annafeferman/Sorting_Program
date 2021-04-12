ccb241717eb707d27502a552e46bb3ff53c15b58
annafefrman <feferman.a@donnu.edu.ua>
2021-04-12 19:22:21 +0300

    Final Commit

---
+++ Interface.py
@@ -0,0 +1,144 @@
+from tkinter import *
+from tkinter import messagebox as mb
+import tkinter.font as font
+import SortingProgramInterface as sp
+
+root = Tk()
+
+
+class Interface:
+    background_color = "#3B5975"
+    text_color = 'White'
+    method_list = ["BubbleSort", "SelectionSort", "InsertionSort", "CocktailSort", "ShellSort"]
+    root.title('Сортування масиву')
+    root.geometry('800x500+420+30')
+    root['bg'] = background_color
+    myFont = font.Font(size=14, weight="bold")
+    array_length_input_field = Entry()
+    array_length_input_label = Label(text='Довжина масиву:', bg=background_color, fg=text_color)
+    method_choice_label = Label(text='Оберіть метод сортування', bg=background_color, fg=text_color)
+    up_down_choice_label = Label(text='Оберіть тип сортування', bg=background_color, fg=text_color)
+    auto_array_input_label = Label(text='Сформуйте масив автоматично', bg=background_color, fg=text_color)
+    auto_array_input_button = Button(text="Сформувати\nта сортувати", height=3, width=12, bg="#AFEEEE", activebackground="#48D1CC", font=myFont, command=lambda:auto_array_sort())
+    manual_array_input_label = Label(text='Або введіть масив самостійно', bg=background_color, fg=text_color)
+    manual_array_input_field = Entry()
+    output_label_1 = Text(width=95, bg="#EDFAFA", height=2)
+    output_label_2 = Text(width=95, bg="#EDFAFA", height=2)
+    output_label_3 = Text(width=95, bg="#EDFAFA", height=2)
+    space = Label(bg=background_color)
+    array_sort_button = Button(text="Сортувати", height=3, width=10, bg="#AFEEEE", activebackground="#48D1CC", font=myFont, command=lambda:on_sort_clicked())
+    output_1_name = Label(text='Вихідний масив: ', bg=background_color, fg=text_color)
+    output_2_name = Label(text='Відсортований масив: ', bg=background_color, fg=text_color)
+    output_3_name = Label(text='Час виконання програми: ', bg=background_color, fg=text_color)
+    restart_button_yes = Button(height=3, width=20, text="Повторити програму", bg="#E0FFFF", activebackground="#AFEEEE", command=lambda:clear())
+    restart_button_no = Button(height=3, width=20, text="Завершити програму", bg="#E0FFFF", activebackground="#AFEEEE", command=lambda:quit())
+    array_length_input_label.grid(row=1, column=2)
+    array_length_input_field.grid(row=2, column=2)
+    method_choice_label.grid(row=0, column=0)
+    up_down_choice_label.grid(row=0, column=1)
+    auto_array_input_label.grid(row=0, column=2)
+    auto_array_input_button.grid(row=3, column=2, rowspan=4, pady=8)
+    manual_array_input_label.grid(row=0, column=3)
+    manual_array_input_field.grid(row=1, column=3)
+    array_sort_button.grid(row=2, column=3, rowspan=3, pady=7)
+    output_1_name.grid(row=8, column=0)
+    output_label_1.grid(row=9, column=0, columnspan=4)
+    output_2_name.grid(row=10, column=0)
+    output_label_2.grid(row=11, column=0, columnspan=4)
+    output_3_name.grid(row=12, column=0)
+    output_label_3.grid(row=13, column=0, columnspan=4)
+    space.grid(row=14, column=0, columnspan=4)
+    restart_button_yes.grid(row=15, column=1, pady=5, columnspan=2)
+    restart_button_no.grid(row=16, pady=5, column=1, columnspan=2)
+    method_choice_list = Listbox(width=15, height=5)
+    method_choice_list.grid(row=1, column=0, rowspan=3)
+    for i in method_list:
+        method_choice_list.insert(0, i)
+    up_down_choice = IntVar()
+    up_down_choice.set(0)
+    option1 = Radiobutton(root, text="За зростанням", variable=up_down_choice, value=0, bg=background_color, fg=text_color)
+    option2 = Radiobutton(root, text="За спаданням", variable=up_down_choice, value=1, bg=background_color, fg=text_color)
+    option1.grid(row=1, column=1)
+    option2.grid(row=2, column=1)
+
+
+def on_sort_clicked():
+    selected_method = Interface.method_choice_list.curselection()
+    selected_up_down = Interface.up_down_choice.get()
+    array = sp.validate_array(Interface.manual_array_input_field.get())
+    flag = validate_data()
+    if flag:
+        Interface.output_label_1.insert(0.0, array)
+        if selected_method == (4,):
+            Interface.output_label_2.insert(0.0, sp.bubbleSort(array, selected_up_down))
+        elif selected_method == (3,):
+            Interface.output_label_2.insert(0.0, sp.selectionSort(array, selected_up_down))
+        elif selected_method == (2,):
+            Interface.output_label_2.insert(0.0, sp.insertionSort(array, selected_up_down))
+        elif selected_method == (1,):
+            Interface.output_label_2.insert(0.0, sp.cocktailSort(array, selected_up_down))
+        elif selected_method == (0,):
+            Interface.output_label_2.insert(0.0, sp.shellSort(array, selected_up_down))
+        Interface.output_label_3.insert(0.0, sp.ExecutionTime())
+    else:
+        if_mistake()
+
+
+def auto_array_sort():
+    selected_method = Interface.method_choice_list.curselection()
+    selected_up_down = Interface.up_down_choice.get()
+    length_valid = sp.validate_array(Interface.array_length_input_field.get())
+    array_length = sp.validate_array_length(Interface.array_length_input_field.get())
+    array = sp.auto_array_input(array_length)
+    flag = validate_data()
+    if flag:
+        Interface.output_label_1.insert(0.0, array)
+        if selected_method == (4,):
+            Interface.output_label_2.insert(0.0, sp.bubbleSort(array, selected_up_down))
+        elif selected_method == (3,):
+            Interface.output_label_2.insert(0.0, sp.selectionSort(array, selected_up_down))
+        elif selected_method == (2,):
+            Interface.output_label_2.insert(0.0, sp.insertionSort(array, selected_up_down))
+        elif selected_method == (1,):
+            Interface.output_label_2.insert(0.0, sp.cocktailSort(array, selected_up_down))
+        elif selected_method == (0,):
+            Interface.output_label_2.insert(0.0, sp.shellSort(array, selected_up_down))
+        Interface.output_label_3.insert(0.0, sp.ExecutionTime())
+    else:
+        if_mistake()
+
+
+def validate_data():
+    length_valid = sp.validate_array(Interface.array_length_input_field.get())
+    array_valid = sp.validate_array(Interface.manual_array_input_field.get())
+    array_length = sp.validate_array_length(Interface.array_length_input_field.get())
+    array = sp.validate_array(Interface.manual_array_input_field.get())
+    sel_m = Interface.method_choice_list.curselection()
+    if sel_m == (0, ) or sel_m == (1, ) or sel_m == (2, ) or sel_m == (3, ):
+        method_is_selected = True
+    else:
+        method_is_selected = False
+    if array and (len(array) == array_length) and length_valid and array_valid and method_is_selected:
+        flag = True
+    else:
+        flag = False
+    return flag
+
+
+def if_mistake():
+    mb.showerror("Error", "You must have input something wrong... \n Try again!")
+
+
+def clear():
+    Interface.array_length_input_field.delete(0, END)
+    Interface.manual_array_input_field.delete(0, END)
+    Interface.output_label_1.delete(0.0, END)
+    Interface.output_label_2.delete(0.0, END)
+    Interface.output_label_3.delete(0.0, END)
+
+
+def quit():
+    root.destroy()
+
+
+root.mainloop()
---
+++ SortingProgramInterface.py
@@ -0,0 +1,107 @@
+from random import randint  # randint для заполнения массива
+import timeit  # timeit для рассчета времени выполнения
+
+
+def validate_array_length(text):
+    try:
+        res = int(text)
+        if res <= 0:
+            return False
+        return res
+    except(TypeError, ValueError):
+        return False
+
+
+def auto_array_input(length):
+    old_list = []
+    for i in range(length):
+        old_list.append(randint(0,100))
+    return old_list
+
+
+def validate_array(text):
+    delimeter = " "
+    split_array = text.split(delimeter)
+    res_array = []
+    try:
+        for number in split_array:
+            array_element = int(number)
+            res_array.append(array_element)
+        return res_array
+    except(TypeError, ValueError):
+        return False
+
+
+def bubbleSort(my_list, up_down):
+    last_item = len(my_list) - 1
+    for k in range(last_item):  # вложенным циклом перебираем все элементы массива
+        for j in range(last_item - k):
+            if (up_down == 0 and my_list[j] > my_list[j + 1]) or (up_down == 1 and my_list[j] < my_list[j + 1]):
+                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+    return my_list
+
+
+def selectionSort(my_list, up_down):
+    for i in range(len(my_list)):
+        min_index = i
+        for j in range(i + 1, len(my_list)):
+            if (up_down == 0 and my_list[j] < my_list[min_index]) or (up_down == 1 and my_list[j] > my_list[min_index]):
+                min_index = j
+        my_list[i], my_list[min_index] = my_list[min_index], my_list[i]
+    return my_list
+
+
+def insertionSort(my_list, up_down):
+    for i in range(1, len(my_list)):
+        k = my_list[i]  # присваиваем ключу значение номера элемента
+        min_index = i - 1  # начинаем отсчет с минимального номера
+        while (up_down == 0 and min_index >= 0 and my_list[min_index] > k) or (
+                up_down == 1 and min_index >= 0 and my_list[
+            min_index] < k):  # сравниваем значение элемента с прерыдущим
+            my_list[min_index + 1] = my_list[min_index]  # меняем, если он больше
+            min_index -= 1
+        my_list[min_index + 1] = k
+    return my_list
+
+
+def cocktailSort(my_list, up_down):
+    for i in range(len(my_list) // 2):
+        swap = False  # присваем значенрие фолз в начале каждой итерации
+        for j in range(1 + i, len(my_list) - i):
+            if (up_down == 0 and my_list[j] < my_list[j - 1]) or (
+                    up_down == 1 and my_list[j] > my_list[j - 1]):  # используем bubble sort
+                my_list[j], my_list[j - 1] = my_list[j - 1], my_list[j]
+                swap = True
+        if not swap:  # если ничего не изменилось, прерываем цикл (элементы отсортированы)
+            break
+        swap = False
+        for j in range(len(my_list) - i - 1, i, -1):  # проходим цикл без последнего элемента
+            if (up_down == 0 and my_list[j] < my_list[j - 1]) or (
+                    up_down == 1 and my_list[j] > my_list[j - 1]):  # так как он уже на своем месте
+                my_list[j], my_list[j - 1] = my_list[j - 1], my_list[j]  # меняем нужные элементы местами
+                swap = True
+        if not swap:  # если ничего не изменилось, прерываем цикл
+            break
+    return my_list
+
+
+def shellSort(my_list, up_down):
+    n = len(my_list)
+    gap = n // 2  # устанавливаем базовый шаг, деля длину массива на 2, потом он будет уменьшатся
+    while gap > 0:  # цикл выполняется до тех пор, пока окончательно не уменьшится шаг
+        for i in range(gap, n):
+            temp = my_list[i]  # добавляем элемент, который уже отсортирован, во временную переменную
+            j = i  # добавляем отсортированные элементы на нужную позицию
+            while (up_down == 0 and j >= gap and my_list[j - gap] > temp) or (
+                    up_down == 1 and j >= gap and my_list[j - gap] < temp):
+                my_list[j] = my_list[j - gap]  # меняем элементы местами
+                j -= gap
+
+            my_list[j] = temp  # возвращаем значения из временной переменной на нужное место
+        gap //= 2
+    return my_list
+
+
+def ExecutionTime():
+    execution_time = timeit.timeit(number=100000)
+    return execution_time
